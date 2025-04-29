7147f35feb00e1311313bc68a255676fcd82eb45
Sami Ben Romdhane <beromdhane@gmail.com>
2025-04-29 16:28:44 +0100

    Update

--- github.sh
+++ github.sh
@@ -3,9 +3,10 @@
 
 
 GITHUB_USERNAME="samibr"
-GITHUB_TOKEN="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"
+GITHUB_TOKEN="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"
 REPO="arch"
 
 git add .
 git commit -m "Update"
 git push https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/$GITHUB_USERNAME/$REPO.git
+
